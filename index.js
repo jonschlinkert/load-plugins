f9cd63ce5e58ce45cecdb24cb1469ef8597917df
jonschlinkert <dev@sellside.com>
2014-08-23 07:27:01 -0400

    fix path logic

--- index.js
+++ index.js
@@ -9,39 +9,43 @@
 
 var path = require('path');
 var resolve = require('resolve-dep');
-var isAbsolute = require('is-absolute');
+var appname = require('app-name');
 var _ = require('lodash');
 
-var names = ['grunt', 'gulp', 'assemble', 'verb', 'handlebars', 'helper', 'plugin'];
-var list = '(' + names.join('|') + ')';
-var re = new RegExp(list + '[^\\\\\/]*', 'g');
-
-function rename(filepath, re) {
-  var name = path.relative(process.cwd(), filepath);
-  if (/node_modules/.test(name)) {
-    name = name.match(re)[0];
-    var res = new RegExp(list + '-');
-    name = name.replace(res, '');
-  } else {
-    name = path.basename(name, path.extname(name));
-  }
-  return name;
-};
+/**
+ * Load plugins using the given `patterns` and `options`.
+ *
+ * @param  {String} `patterns`
+ * @param  {Object} `options`
+ *     @option {Object} [options] `options`
+ * @return {Object} Returns an object of loaded plugins.
+ */
 
 module.exports = function (patterns, options) {
   var opts = _.defaults({}, options, {
     strict: false, // resolve-dep
     require: false,
-    regex: re
+    strip: ['grunt', 'gulp', 'assemble', 'verb', 'handlebars', 'helper', 'plugin'],
+    rename: rename
   });
 
   var plugins = {};
-
   resolve(patterns, opts).forEach(function (filepath) {
-    var name = rename(filepath, opts.regex);
+    var name = opts.rename(filepath, opts.strip);
     plugins[name] = opts.require ? require(filepath) : filepath;
   });
-
   return plugins;
 };
 
+
+function rename(filepath, names) {
+  if (/node_modules/.test(filepath)) {
+    var dir = path.relative(process.cwd(), filepath);
+    dir = dir.split(/[\\\/]+/g);
+    dir.shift();
+    filepath = dir[0];
+  } else {
+    filepath = path.basename(filepath, path.extname(filepath));
+  }
+  return appname(filepath, names);
+}
